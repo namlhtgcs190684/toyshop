95bcfa1ff8bc23fce07183b12a161b978f826d66
namlhtgcs190684 <71746846+namlhtgcs190684@users.noreply.github.com>
2020-11-06 21:52:57 +0700

    Update 41.00-server (1).js

--- 41.00-server (1).js
+++ 41.00-server (1).js
@@ -4,7 +4,7 @@
 
 const express = require("express")
 const app = express();
-const PORT = 8080;
+const port = process.env.PORT || 8080;
 
 ///----------------------------------------------------
 app.get( '/', (req, res) => indexPage(req, res) );
