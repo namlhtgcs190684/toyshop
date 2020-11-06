5cf30052da497274eea0ac4b59999d7c8f639093
namlhtgcs190684 <71746846+namlhtgcs190684@users.noreply.github.com>
2020-11-06 20:56:33 +0700

    Add files via upload

---
+++ configs/config_DB.js
@@ -0,0 +1,16 @@
+module.exports = {
+    localdb: {
+        urldb : 'mongodb://localhost:27017/toyshop',
+        dbname : "toyshop",
+        dbusername : "",
+        dbpassword : "",
+    },
+
+    clouddb: {
+        urldb : 'mongodb://localhost:27017/toyshop',
+        dbname : "toyshop",
+        dbusername : "",
+        dbpassword : "",
+    }
+
+};
---
+++ configs/config_Header.js
@@ -0,0 +1,12 @@
+module.exports = [
+    { name: "Home" , link: "/"},
+    { name: "Product" , link: "/product"},
+    { name: "Order" , link: "/order"},
+    { name: "Payment" , link: "/payment"},
+    { name: "Report" , link: "/report"},
+    { name: "Login" , link: "/login"},
+    { name: "Feedback" , link: "/Feedback"},
+    { name: "create User" , link: "/user/create"},
+    { name: "create Product" , link: "/product/create"},
+    { name: "QR code - link" , link: "/qr"},
+];
