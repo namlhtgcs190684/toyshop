091751e95c4239ac72e5bed790c9d70553c48d17
namlhtgcs190684 <71746846+namlhtgcs190684@users.noreply.github.com>
2020-11-08 13:23:53 +0700

    Update 71.0.mvc.js

--- 71.0.mvc.js
+++ 71.0.mvc.js
@@ -26,7 +26,13 @@ var atob = require('atob');
 /// ------------------ CONFIG
 var configHeader = require("./configs/config_Header");
 var configDB = require("./configs/config_DB");
-const PORT = process.env.PORT;
+var http = require('http');
+var port = Number(process.env.PORT || 8080);
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/plain'});
+res.end('71.0.mvc.js\n');
+}).listen(port);
+console.log('Server running');
 var urldb = configDB.localdb.urldb;
 
 
